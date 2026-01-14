82965eacffd8f76aa924f2056a67f05c6e079b96
Oussama Qarbach <61934836+OussamaQarbach@users.noreply.github.com>
2026-01-14 13:47:55 +0100

    Fix: Explicitly import Prisma Client from client.ts to resolve import errors

--- actions/newsletter.ts
+++ actions/newsletter.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { PrismaClient } from "@/lib/generated/client";
+import { PrismaClient } from "@/lib/generated/client/client";
 
 const prisma = new PrismaClient();
 
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../lib/generated/client'
+import { PrismaClient } from '../lib/generated/client/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import pg from 'pg'
 import 'dotenv/config'
